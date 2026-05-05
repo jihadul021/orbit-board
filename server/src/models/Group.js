e70ba348b2aac75b4bb9c8e9312630b5b359805e
jihadul021 <jihadul021@gmail.com>
2026-05-05 21:29:11 +0600

    feat: added models article,board,group,list,user

---
+++ server/src/models/Group.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const memberSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { _id: false })
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [memberSchema]
+}, { timestamps: true })
+
+const Group = mongoose.model('Group', groupSchema)
+
+export default Group
